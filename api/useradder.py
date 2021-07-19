9299d1138a1416f68eaee2bc34f4ae86c9bf34d4
IFOX-COMPANY <85041583+IFOX-COMPANY@users.noreply.github.com>
2021-07-19 06:31:19 -0400

    -+

--- api/useradder.py
+++ api/useradder.py
@@ -1,13 +1,11 @@
 from tkinter import *
-from json import *
+import time
 
-f = open('textfile.txt', 'r+')
 window = Tk()
 userlbl = Label(window, text="Player Name")
 userentry = Entry(window)
 capelbl = Label(window, text="Cape URL")
 capeentry = Entry(window)
-#add = Button(window, text ="Add Player Cape", command=addcape)
 
 userlbl.pack()
 userentry.pack()
@@ -15,6 +13,23 @@ capelbl.pack()
 capeentry.pack()
 
 def addcape():
-    print(f.read())
-    f.write(f.read() - 1 + userentry.get() + ": " + capeentry.get())
+    f = open('capes.json', 'r+')
+    contents = f.read().replace("{\n", "")
+    contents = contents.replace("\n}", "")
+    contents = contents.replace("", "")
+    contents = contents + "," + "\n"
+    print(contents)
+    f.write(f.read().replace("\n}", ""))
+    f.close
+    quotefile = open('quote.txt')
+    quote = quotefile.read()
+    quotefile.close()
+    f = open('capes.json', 'r+')
+    f.write("{\n" + contents + "    " + quote + userentry.get() + quote + ": " + quote + capeentry.get() + quote + "\n}")
     f.close()
+
+        
+    
+button = Button(window, text="Add Player Cape", command=addcape)
+button.pack()
+input('Press any key to finish')
